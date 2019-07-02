565de8e40b3cc9f2e84e716864c2b375e5592f66
romana98 <romana_1998@hotmail.com>
2019-07-02 12:24:38 +0200

    metoda za upis u fajlove

--- WorkShop/src/controller/Main.java
+++ WorkShop/src/controller/Main.java
@@ -1,11 +1,32 @@
 package controller;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import model.Aplikacija;
 
 import view.*;
 
 public class Main {
 
 	public static void main(String[] args) {
+		ArrayList<String> fajlovi = new ArrayList<String>();
+		
+		fajlovi.add("Grad"); fajlovi.add("Artikl"); fajlovi.add("Korisnik"); 
+		fajlovi.add("Kategorija"); fajlovi.add("Prodavnica"); fajlovi.add("Porudzbina");
+		
+		Aplikacija app = new Aplikacija(fajlovi);
+		
 		MainWindow ww = new MainWindow();
+		
+		try
+		{
+			app.sacuvajUFajl(fajlovi);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	
 	}
 
