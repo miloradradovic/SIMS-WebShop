55b0f5019b9c0f4239db38a52955f0e44e6e98e9
miloradradovic <45543741+miloradradovic@users.noreply.github.com>
2019-07-05 16:24:19 +0200

    Citanje podataka iz fajla. Fali jos citanje kategorija i stanja

--- WorkShop/src/model/Aplikacija.java
+++ WorkShop/src/model/Aplikacija.java
@@ -1,10 +1,14 @@
 package model;
 import java.io.*;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 import classes.*;
+import enums.Boja;
+import enums.Pol;
 import enums.TipKorisnika;
+import states.Stanje;
 
 /***********************************************************************
  * Module:  Aplikacija.java
@@ -24,6 +28,7 @@ public class Aplikacija {
    public ArrayList<Korpa> korpe;
    public ArrayList<Kategorija> kategorije;
    public ArrayList<Prodavnica> prodavnice;
+   public ArrayList<Porudzbina> porudzbine;
    
    public SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy.");
    
@@ -32,8 +37,14 @@ public class Aplikacija {
    public Aplikacija(ArrayList<String> naziviFajlova)
    {
 	   //TODO Mico, good luck
-	   //Mico, ovde samo pozivas metodu koju dole implementiras, napisao sam ti gde
-	   //ucitajIzFajla();
+	   try{
+		   ucitajIzFajla(naziviFajlova); 
+	   }
+	   catch(IOException e){
+		   e.printStackTrace();
+	   }catch(ParseException e){
+		   e.printStackTrace();
+	   }
    }
    
    public Aplikacija(TipKorisnika aktivniKorisnik, int id, ArrayList<Grad> grad, ArrayList<Artikl> artikl,
@@ -268,9 +279,172 @@ public class Aplikacija {
 		this.id = id;
 	}
 	   
-	public void ucitajIzFajla(ArrayList<String> naziviFajlova)
+	public void ucitajIzFajla(ArrayList<String> naziviFajlova) throws IOException, ParseException
 	{
 		//TODO: Mico ovde ide tvoja implementacija ucitavanja iz fajla.
+		for(String fajl:naziviFajlova){
+			if (fajl.equals("Grad")){
+				BufferedReader br = new BufferedReader(new FileReader(".\\Files\\"+fajl+".txt"));
+				String temp;
+				while((temp = br.readLine())!= null){
+					Grad grad = new Grad();
+					String[] delimiter = temp.split("\\|");
+					grad.setMesto(delimiter[0]);
+					grad.setPostanskiBroj(Integer.parseInt(delimiter[1]));
+					gradovi.add(grad);
+				}
+				br.close();
+			}else if(fajl.equals("Kategorija")){
+				BufferedReader br = new BufferedReader(new FileReader(".\\Files\\"+fajl+".txt"));
+				String temp;
+				HashMap<Kategorija,String[]> mapa = new HashMap<Kategorija,String[]>();
+				while((temp=br.readLine())!=null){
+					Kategorija kategorija = new Kategorija();
+					String[]delimiter = temp.split("\\|");
+					kategorija.setNaziv(delimiter[0]);
+					kategorije.add(kategorija);
+					String[] delimiter2 = delimiter[1].split("\\,");
+					mapa.put(kategorija, delimiter2);
+				}
+			}else if(fajl.equals("Artikl")){
+				BufferedReader br = new BufferedReader(new FileReader(".\\Files\\"+fajl+".txt"));
+				String temp;
+				while((temp=br.readLine())!=null){
+					Artikl artikl = new Artikl();
+					String[] delimiter = temp.split("\\|");
+					artikl.setSifra(delimiter[0]);
+					artikl.setSlika(delimiter[1]);
+					artikl.setNaziv(delimiter[2]);
+					artikl.setStanje(Integer.parseInt(delimiter[3]));
+					artikl.setCena(Integer.parseInt(delimiter[4]));
+					artikl.setBoja(Boja.valueOf(delimiter[5]));
+					artikl.setAktivan(Boolean.parseBoolean(delimiter[6]));
+					for(Kategorija k:kategorije){
+						if(k.getNaziv().equals(delimiter[7])){
+							artikl.setKategorija(k);
+						}
+					}
+					artikli.add(artikl);
+				}
+				br.close();
+			}else if(fajl.equals("Korisnik")){
+				BufferedReader br = new BufferedReader(new FileReader(".\\Files\\"+fajl+".txt"));
+				String temp;
+				while((temp=br.readLine())!=null){
+					Korisnik k = new Korisnik();
+					String[] delimiter = temp.split("\\|");
+					k.setEmail(delimiter[0]);
+					k.setTelefon(delimiter[1]);
+					k.setIme(delimiter[2]);
+					k.setPrezime(delimiter[3]);
+					k.setJmbg(Integer.parseInt(delimiter[4]));
+					k.setPol(Pol.valueOf(delimiter[5]));
+					k.setKorisnickoIme(delimiter[6]);
+					k.setSifra(delimiter[7]);
+					k.setDatumRodj(format.parse(delimiter[8]));
+					String[] delimiter2 = delimiter[9].split("\\%");
+					Adresa a = new Adresa();
+					a.setUlica(delimiter2[0]);
+					a.setBroj(Integer.parseInt(delimiter2[1]));
+					for(Grad g:gradovi){
+						if(g.getPostanskiBroj()==Integer.parseInt(delimiter2[2])){
+							a.setGrad(g);
+						}
+					}
+					k.setAdresa(a);
+					ArrayList<Porudzbina> porudzbineK = new ArrayList<Porudzbina>();
+					String[] delimiter3 = delimiter[10].split("\\,");
+					for(int i = 0;i<delimiter3.length;i++){
+						for(Porudzbina p:porudzbine){
+							if(p.getBrojPor()==Integer.parseInt(delimiter3[i])){
+								porudzbineK.add(p);
+							}
+						}
+					}
+					k.setPorudzbina(porudzbineK);
+					String[] delimiter4 = delimiter[11].split("\\,");
+					ArrayList<Artikl> listaZeljaK = new ArrayList<Artikl>();
+					for(int i = 0;i<delimiter4.length;i++){
+						for(Artikl a1:artikli){
+							if(a1.getSifra().equals(delimiter4[i])){
+								listaZeljaK.add(a1);
+							}
+						}
+					}
+					k.setArtikl(listaZeljaK);
+					String[] delimiter5 = delimiter[12].split("\\,");
+					ArrayList<Stavka> stavkeK = new ArrayList<Stavka>();
+					for(int i = 0;i<delimiter5.length;i++){
+						String[] delimiter6 = delimiter5[i].split("\\%");
+						for(Artikl a2:artikli){
+							if(a2.getSifra().equals(delimiter6[0])){
+								Stavka s = new Stavka();
+								s.setArtikl(a2);
+								s.setKolicina(Integer.parseInt(delimiter6[1]));
+								stavkeK.add(s);
+							}
+						}
+					}
+					Korpa korpa = new Korpa();
+					korpa.setStavka(stavkeK);
+					k.setKorpa(korpa);
+				}
+				br.close();
+			}else if(fajl.equals("Prodavnica")){
+				BufferedReader br = new BufferedReader(new FileReader(".\\Files\\"+fajl+".txt"));
+				
+				String temp;
+				while((temp=br.readLine())!=null){
+					Prodavnica p = new Prodavnica();
+					String[] delimiter = temp.split("\\|");
+					p.setPocetakRadVr(delimiter[0]);
+					p.setKrajRadVr(delimiter[1]);
+					p.setNaziv(delimiter[2]);
+					p.setEmail(delimiter[3]);
+					String[] delimiter2 = delimiter[4].split("\\,");
+					HashMap<Artikl,Integer> raspolozivost = new HashMap<Artikl,Integer>();
+					for(int i = 0;i<delimiter2.length;i++){
+						String[]delimiter3 = delimiter2[i].split("\\%");
+						for(Artikl a3:artikli){
+							if(a3.getSifra().equals(delimiter3[0])){
+								raspolozivost.put(a3, Integer.parseInt(delimiter3[1]));
+							}
+						}
+					}
+					p.setRaspolozivo(raspolozivost);
+					String[] delimiter4 = delimiter[5].split("\\%");
+					Adresa adresa = new Adresa();
+					adresa.setUlica(delimiter4[0]);
+					adresa.setBroj(Integer.parseInt(delimiter4[1]));
+					for(Grad g:gradovi){
+						if(g.getPostanskiBroj()==Integer.parseInt(delimiter4[2])){
+							adresa.setGrad(g);
+						}
+					}
+					p.setAdresa(adresa);
+				}
+				br.close();
+			}else if(fajl.equals("Porudzbina")){
+				BufferedReader br = new BufferedReader(new FileReader(".\\Files\\"+fajl+".txt"));
+				
+				String temp;
+				while((temp=br.readLine())!=null){
+					Porudzbina p = new Porudzbina();
+					String[] delimiter = temp.split("\\|");
+					p.setBrojPor(Integer.parseInt(delimiter[0]));
+					p.setUkupnaCena(Integer.parseInt(delimiter[1]));
+					p.setDatumPorucivanja(format.parse(delimiter[2]));
+					String[] delimiter2 = delimiter[3].split("\\,");
+					HashMap<String,Integer> mapa = new HashMap<String,Integer>();
+					for(int i = 0;i<delimiter2.length;i++){
+						String[] delimiter3 = delimiter2[i].split("\\%");
+						mapa.put(delimiter3[0], Integer.parseInt(delimiter3[1]));
+					}
+					p.setCene(mapa);
+					//NISAM DODAO STANJE JER NZM :)
+				}
+			}
+		}
 	}
 	
 	public void sacuvajUFajl(ArrayList<String> naziviFajlova) throws IOException
